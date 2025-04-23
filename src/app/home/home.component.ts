13fed23a35af5ec48aba66b19dabed5873359711
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2025-04-23 10:02:38 +0200

    fixed a minor design flaw on low resulutions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { JoinHeaderComponent } from '../shared/join-header/join-header.component';
+import {ContactsService} from "../shared/services/backend/contacts.service";
+import {NgClass, NgStyle} from "@angular/common";
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [RouterModule, NavbarComponent, JoinHeaderComponent],
+  imports: [RouterModule, NavbarComponent, JoinHeaderComponent, NgClass, NgStyle],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  getBackgroundColor() {
+    return `background-color: var(--greyBG);`;
+  }
+}
