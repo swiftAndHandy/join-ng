a1a1e9ff4dfaea63de28ef9d6c78cd540e8b4a2e
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2025-04-24 18:08:25 +0200

    fixed another bunch of design flaws

--- src/app/home/summary/summary.component.ts
+++ src/app/home/summary/summary.component.ts
@@ -8,4 +8,20 @@ import { SummaryDetailsComponent } from './summary-details/summary-details.compo
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss',
 })
-export class SummaryComponent {}
+export class SummaryComponent {
+  greet() {
+    const hour = new Date().getHours();
+    if (hour >= 5 && hour < 12) {
+      return 'Good morning\n';
+    } else if (hour >= 12 && hour < 17) {
+      return 'Hello\n';
+    } else if (hour >= 17 && hour < 22) {
+      return 'Good evening,\n';
+    }
+    return 'Good night,\n';
+  }
+
+  user() {
+    return 'guest'
+  }
+}
